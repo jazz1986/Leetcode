707a0b4e78387bdfc11811e504c2e7a3548773bb
jazz1986 <programkrm@gmail.com>
2022-01-14 19:30:48 +0200

    задача из литкода

---
+++ untitled6/src/main.java
@@ -0,0 +1,19 @@
+
+
+public class Sqrt{
+
+    public static void main(String[] args) {
+
+        int x= 8;
+        double y=Math.sqrt(x);
+        int value = (int)y;
+        System.out.println(value);
+
+      }
+    }
+
+
+
+
+
+
